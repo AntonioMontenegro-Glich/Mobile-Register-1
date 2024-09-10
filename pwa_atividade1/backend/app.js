77ecaa589ca7304c438e5042be5ce143de7545b8
unknown <antoniolemos2004@gmail.com>
2024-09-09 21:02:33 -0300

    Correção nas rotas e servidor(app.js)

--- pwa_atividade1/backend/app.js
+++ pwa_atividade1/backend/app.js
@@ -1,18 +1,27 @@
-
-const mongoose = require("mongoose");
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require ('dotenv').config();
 const alunosRoute = require("./routes/alunos");
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+//Middleware
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'})); // Aumentado o Limite de tamanho
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));//Para dados enviados via URL encoded
+app.use(express.static('public'));
 
+//Conexão com o MongoDb
 mongoose.connect(process.env.MONGODB_URI);
+    .then( () => console.log('MongoDb Connected'))
+    .catch (err => console.log(err));
 
+//Rotas
+const alunosRoute = require('./routes/alunos');
 app.use('/api/alunos', alunosRoute);
 
+//Porta
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server está rodando na porta ${port}`));
